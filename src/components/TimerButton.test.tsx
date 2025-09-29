0b459667e2ab8c7b9bbc33ac703cf4032ee256e3
James McDonald <jamesmcdonald112@yahoo.ie>
2025-09-29 12:15:58 +0100

    test: commit failing test

--- src/components/TimerButton.test.tsx
+++ src/components/TimerButton.test.tsx
@@ -1,16 +1,14 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import TimerButton from "./TimerButton";
+import { fireEvent, render, screen } from '@testing-library/react'
+import TimerButton from './TimerButton'
 
-test("renders Start button to the screen", () => {
-  const handleClick = vi.fn();
-  render(<TimerButton toggleStart={handleClick} label="Start"/>);
+test('renders Start button to the screen', () => {
+  const handleClick = vi.fn()
+  render(<TimerButton toggleStart={handleClick} label="Start" />)
 
-  const startButton = screen.getByRole('button', {name: /start/i});
+  const startButton = screen.getByRole('button', { name: /star/i })
 
-  expect(startButton).toBeInTheDocument();
+  expect(startButton).toBeInTheDocument()
 
-  fireEvent.click(startButton);
-  expect(handleClick).toHaveBeenCalledTimes(1);
-
-  
+  fireEvent.click(startButton)
+  expect(handleClick).toHaveBeenCalledTimes(1)
 })
